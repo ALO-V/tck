c66d7023e30a8ba46dbd0cf6a1088da59088990f
themiloagency <themiloagency@gmail.com>
2018-06-06 09:48:58 -0400

    Update hoursdirections.php

--- public/Information/hoursdirections.php
+++ public/Information/hoursdirections.php
@@ -56,7 +56,7 @@
         <section id="hdIT" class="interiorContainer">
         <table id="hoursTable">
         <thead class="hoursHeaderRow">
-          <th class="homeHeaderText" colspan="2" style="border-top: none;">Book today! <span style="font-size:1em;">515-344-4286<span></th>
+          <th class="homeHeaderText" colspan="2" style="border-top: none;">Book today! <span style="font-size:.8em;"><i>515-344-4286</i><span></th>
         </thead>
         <tbody>
         <tr>
