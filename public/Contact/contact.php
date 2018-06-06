e4d10adc7775a929dc3136ddeae7862f53a718a1
themiloagency <themiloagency@gmail.com>
2018-06-06 09:48:29 -0400

    Update contact.php

--- public/Contact/contact.php
+++ public/Contact/contact.php
@@ -175,7 +175,7 @@ if(isset($_POST['submit'])) {
         <section class="interiorContainer">
         <table id="hoursTable">
         <thead class="hoursHeaderRow">
-          <th class="homeHeaderText" colspan="2" style="border-top: none;">Book today! <span style="font-size:1em;">515-344-4286<span></th>
+          <th class="homeHeaderText" colspan="2" style="border-top: none;">Book today! <span style="font-size:.8em;"><i>515-344-4286</i><span></th>
         </thead>
         <tbody>
         <tr>
